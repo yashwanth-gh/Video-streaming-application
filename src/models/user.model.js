ccbfd937fa4c7a5af4f4be866666fe4aefafea5a
Yashwanth B M <yashwanthbm36@gmail.com>
2024-01-25 13:45:05 +0530

    added jwt and bcrypt

---
+++ src/models/user.model.js
@@ -0,0 +1,112 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema = Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // cloudinary url
+      required: true,
+    },
+    coverImage: {
+      type: String, // cloudinary url
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required!"],
+    },
+    refreshToken: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+/**
+ * Middleware function that is executed before saving a user document.
+ * It hashes the password if it has been modified.
+ *
+ * @param {Function} next - The next function to be called in the middleware chain.
+ * @returns {void}
+ */
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+/**
+ * Checks if the provided password is correct for the user.
+ * @param {string} password - The password to check.
+ * @returns {Promise<boolean>} - A promise that resolves to a boolean indicating if the password is correct.
+ */
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password,this.password);
+}
+
+/**
+ * Generates an access token for the user.
+ * @returns {string} The generated access token.
+ */
+userSchema.methods.generateAccessToken = function(){
+    jwt.sign(
+        {
+        _id : this._id,
+        email : this.email,
+        username : this.username,
+        fullName : this.fullName,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+};
+
+/**
+* Generates a refresh token for the user.
+* @returns {string} The generated refresh token.
+*/
+userSchema.methods.generateRefreshToken = function(){
+    jwt.sign(
+        {
+        _id : this._id,
+
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+};
+
+export const User = mongoose.model("User", userSchema);
---
+++ src/models/video.model.js
@@ -0,0 +1,44 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = Schema(
+    {
+        videoFile : {
+            type : String,  // cloudinary url
+            required : true
+        },
+        thumbnail : {
+            type : String,  // cloudinary url
+            required : true
+        },
+        title : {
+            type : String,  // cloudinary url
+            required : true
+        },
+        description : {
+            type : String,  // cloudinary url
+            required : true
+        },
+        duration : {
+            type : Number,  // cloudinary url
+            required : true
+        },
+        views : {
+            type : Number,  // cloudinary url
+            default : 0
+        },
+        isPublished : {
+            type : Boolean,  // cloudinary url
+            default : true
+        },
+        owner : {
+            type : Schema.Types.ObjectId,
+            ref: "User"
+        }
+        
+    },{
+        timestamps : true
+    }
+)
+videoSchema.plugin(mongooseAggregatePaginate);
+export const Video = mongoose.model("Video",videoSchema);
