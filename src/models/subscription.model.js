38cac4ee63b06ef35a0579739d87f42bb19cbdfe
Yashwanth B M <yashwanthbm36@gmail.com>
2024-02-04 23:46:46 +0530

    added subscribtion model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const subscriptionSchema = mongoose.Schema(
+    {
+        subscriber : {
+            type:mongoose.Schema.Types.ObjectId, //one who is subscribing
+            ref:"User"
+        },
+        channel :{
+            type:mongoose.Schema.Types.ObjectId,//one to whomm 'subscriber' is subscribing
+            ref:"User"
+        }
+    },{
+        timestamps:true
+    }
+)
+
+export const Subscription = mongoose.model("Subscription",subscriptionSchema);
